6f83a52cee9491363d655bf38bfe7a431fb460f1
chiaracc <chiaracaldinelli@gmail.com>
2021-01-06 18:00:23 +0000

    cc in title

--- flexiblehubsrunPoolSubjs.sh
+++ flexiblehubsrunPoolSubjs.sh
@@ -1,10 +1,10 @@
 #!/bin/bash
 
 PYTHON="/camcan/anaconda3/envs/chiara_env/bin/python"
-subjdir=/camcan/studyforrest/studyforrest/subjlistSF.txt
+subjdir=/camcan/studyforrest/studyforrest/subjlistCC.txt
 
 for chunklen in 15 30 45 60 90 120
 do
   echo "Requesting chunklen $chunklen for subject $subj"
-  sbatch --export=CHUNKLEN=$chunklen,SUBJ=$subj /camcan/studyforrest/studyforrest/flexiblehubs_1jobPoolSubjs.sh 
+  sbatch --export=CHUNKLEN=$chunklen,SUBJ=$subj /camcan/studyforrest/studyforrest/flexiblehubs_1jobPoolSubjsCC.sh 
 done
