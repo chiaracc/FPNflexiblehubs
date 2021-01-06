ec752e8b74c8f134d56c997c9145d12bcec459c7
chiaracc <chiaracaldinelli@gmail.com>
2021-01-06 18:05:55 +0000

    Update flexiblehubsrunPoolSubjs.sh

--- flexiblehubsrunPoolSubjs.sh
+++ flexiblehubsrunPoolSubjs.sh
@@ -5,6 +5,6 @@ subjdir=/camcan/studyforrest/studyforrest/subjlistCC.txt
 
 for chunklen in 15 30 45 60 90 120
 do
-  echo "Requesting chunklen $chunklen for subject $subj"
-  sbatch --export=CHUNKLEN=$chunklen,SUBJ=$subj /camcan/studyforrest/studyforrest/flexiblehubs_1jobPoolSubjsCC.sh 
+  echo "Requesting chunklen $chunklen"
+  sbatch --export=CHUNKLEN=$chunklen /camcan/studyforrest/studyforrest/flexiblehubs_1jobPoolSubjsCC.sh 
 done
