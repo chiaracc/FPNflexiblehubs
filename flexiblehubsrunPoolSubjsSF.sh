39fa9fc923e3cb26e423b768e78b3f08902ffa27
chiaracc <chiaracaldinelli@gmail.com>
2021-01-07 17:16:59 +0000

    Create flexiblehubsrunPoolSubjsSF.sh

---
+++ flexiblehubsrunPoolSubjsSF.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+PYTHON="/camcan/anaconda3/envs/chiara_env/bin/python"
+subjdir=/camcan/FPNflexiblehubs/FPNflexiblehubs/subjlistCC.txt
+
+for chunklen in 15 30 45 60 90 120
+do
+  echo "Requesting chunklen $chunklen"
+  sbatch --export=CHUNKLEN=$chunklen /camcan/FPNflexiblehubs/FPNflexiblehubs/flexiblehubs_1jobPoolSubjsSF.sh 
+done
