573549db736ba0abb04dc1759028722132a94327
chiaracc <chiaracaldinelli@gmail.com>
2021-01-12 22:09:07 +0000

    Create flexiblehubs_1jobSF.sh

---
+++ flexiblehubs_1jobSF.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+#SBATCH -J CC
+#SBATCH --cpus-per-task=8
+#SBATCH --output=/camcan/results_rep/camcan/slurm-%j.out
+#SBATCH --error=/camcan/results_rep/camcan/slurm-%j.err
+
+PYTHON="/camcan/anaconda3/envs/chiara_env/bin/python" 
+
+echo "In flexiblehubs_1job parameters CHUNKLEN=$CHUNKLEN and SUBJ=$SUBJ"
+${PYTHON} /camcan/studyforrest/studyforrest/flexibehubsrunCC.py $CHUNKLEN $SUBJ
