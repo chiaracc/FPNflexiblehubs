cfd17c07ddc012d83444f3ef4813d23b32724604
chiaracc <chiaracaldinelli@gmail.com>
2021-01-12 22:07:49 +0000

    loop subj and chunk

--- flexiblehubsrunCC.sh
+++ flexiblehubsrunCC.sh
@@ -1,10 +1,13 @@
 #!/bin/bash
 
 PYTHON="/camcan/anaconda3/envs/chiara_env/bin/python"
-subjdir=/camcan/FPNflexiblehubs/FPNflexiblehubs/subjlistCC.txt
+subjdir=/camcan/studyforrest/studyforrest/subjlistSF.txt
 
 for chunklen in 15 30 45 60 90 120
 do
-  echo "Requesting chunklen $chunklen"
-  sbatch --export=CHUNKLEN=$chunklen /camcan/FPNflexiblehubs/FPNflexiblehubs/flexiblehubs_1jobCC.sh 
+  for subj in `cat ${subjdir}`
+  do
+    echo "Requesting chunklen $chunklen for subject $subj"
+    sbatch --export=CHUNKLEN=$chunklen,SUBJ=$subj /camcan/studyforrest/studyforrest/flexiblehubs_1job.sh  
+  done
 done
