418246573ec04954115493f342483211dcb505b9
chiaracc <chiaracaldinelli@gmail.com>
2021-01-13 22:28:49 +0000

    only 60 chunk

--- flexiblehubsrunPoolSubjsCC.sh
+++ flexiblehubsrunPoolSubjsCC.sh
@@ -3,7 +3,8 @@
 PYTHON="/camcan/anaconda3/envs/chiara_env/bin/python"
 subjdir=/camcan/FPNflexiblehubs/FPNflexiblehubs/subjlistCC.txt
 
-for chunklen in 15 30 45 60 90 120
+#for chunklen in 15 30 45 60 90 120
+for chunklen in 60
 do
   echo "Requesting chunklen $chunklen"
   sbatch --export=CHUNKLEN=$chunklen /camcan/FPNflexiblehubs/FPNflexiblehubs/flexiblehubs_1jobPoolSubjsCC.sh 
