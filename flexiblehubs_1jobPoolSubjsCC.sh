885803ab4a0db599a87979d3b60fb001aaf5908e
chiaracc <chiaracaldinelli@gmail.com>
2021-01-14 22:13:57 +0000

    save in /FPNflexiblehubs/

--- flexiblehubs_1jobPoolSubjsCC.sh
+++ flexiblehubs_1jobPoolSubjsCC.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 #SBATCH -J FH-CC
 #SBATCH --cpus-per-task=8
-#SBATCH --output=/camcan/results_rep/camcan/slurm-%j.out
-#SBATCH --error=/camcan/results_rep/camcan/slurm-%j.err
+#SBATCH --output=/camcan/FPNflexiblehubs/FPNflexiblehubs/lost_res/camcan/slurm-%j.out
+#SBATCH --error=/camcan/FPNflexiblehubs/FPNflexiblehubs/lost_res/camcan/slurm-%j.err
 
 PYTHON="/camcan/anaconda3/envs/chiara_env/bin/python"
 
