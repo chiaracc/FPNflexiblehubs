f2a0140ef197e3c53965561afb6190831fd972a7
chiaracc <chiaracaldinelli@gmail.com>
2021-01-14 22:12:45 +0000

    rm 120

--- flexiblehubsrunPoolSubjsCC.sh
+++ flexiblehubsrunPoolSubjsCC.sh
@@ -3,8 +3,7 @@
 PYTHON="/camcan/anaconda3/envs/chiara_env/bin/python"
 subjdir=/camcan/FPNflexiblehubs/FPNflexiblehubs/subjlistCC.txt
 
-#for chunklen in 15 30 45 60 90 120
-for chunklen in 60
+for chunklen in 15 30 45 60 90 
 do
   echo "Requesting chunklen $chunklen"
   sbatch --export=CHUNKLEN=$chunklen /camcan/FPNflexiblehubs/FPNflexiblehubs/flexiblehubs_1jobPoolSubjsCC.sh 
