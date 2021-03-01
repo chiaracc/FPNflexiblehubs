2fcbfd645fbc70cccf13e022c8a6187ac86a33e0
chiaracc <chiaracaldinelli@gmail.com>
2021-03-01 19:08:03 +0000

    Update flexiblehubs.py

--- flexiblehubs.py
+++ flexiblehubs.py
@@ -196,7 +196,7 @@ class FlexibleHubs:
                                     
                 # Save results in the subject's folder
                 print('this is allresults:')
-                print(allresults)
+                #print(allresults)
                 np.save("allresults_reg_%d_%s"%(chunklen,subj) + ".npy",allresults)
                 print(os.path.join(self.datapth,"allresults_reg_%d_%s"%(chunklen,subj) + ".npy"))
 
