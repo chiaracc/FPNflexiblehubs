f7e993f120f3fd544bc373780a5f3e49e4794ba4
chiaracc <chiaracaldinelli@gmail.com>
2021-03-18 19:15:08 +0000

    Update flexibehubsrunCC.py

--- flexibehubsrunCC.py
+++ flexibehubsrunCC.py
@@ -43,7 +43,7 @@ else:
     subj=(sys.argv[2])
 
 # Run stage 1
-studyforrest.stage1_analysis(chunklen, subj, corr=0)
+#studyforrest.stage1_analysis(chunklen, subj, corr=0)
 
 # Run stage 2
 studyforrest.stage2_analysis(chunklen, subj, corr=0)
